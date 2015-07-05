2eaa861d9ecb81d755cd1ca30575e9499e6ea9e4
zlepper <hansen13579@gmail.com>
2015-07-05 18:08:37 +0200

    Disable logger

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-/*
+
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -63,6 +63,6 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
-});*/
+});
 
 module.exports = app;
